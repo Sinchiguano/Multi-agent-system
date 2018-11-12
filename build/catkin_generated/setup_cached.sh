fa14c933c0e6fd58f9e91923b81cbb1979e77d69
Sinchiguano <cesarsinchiguano@hotmail.es>
2018-11-12 21:12:48 +0100

    report

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -5,6 +5,16 @@
 # it emulates the modifications of the setup script without recurring computations
 
 # new environment variables
+export TURTLEBOT_GAZEBO_MAP_FILE="/home/casch/turtle/src/turtlebot_simulator/turtlebot_gazebo/maps/playground.yaml"
+export TURTLEBOT_GAZEBO_WORLD_FILE="/home/casch/turtle/src/turtlebot_simulator/turtlebot_gazebo/worlds/playground.world"
+export TURTLEBOT_STAGE_MAP_FILE="/home/casch/turtle/src/turtlebot_simulator/turtlebot_stage/maps/maze.yaml"
+export TURTLEBOT_STAGE_WORLD_FILE="/home/casch/turtle/src/turtlebot_simulator/turtlebot_stage/maps/stage/maze.world"
+export TURTLEBOT_STDR_MAP_FILE="/home/casch/turtle/src/turtlebot_simulator/turtlebot_stdr/maps/sparse_obstacles.yaml"
 
 # modified environment variables
+export CMAKE_PREFIX_PATH="/home/casch/turtle/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/casch/turtle/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/casch/turtle/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/casch/turtle/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/casch/turtle/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/casch/turtle/src:$ROS_PACKAGE_PATH"
---
+++ build/tf_turtles/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/casch/turtle/src/tf_turtles
+# Build directory: /home/casch/turtle/build/tf_turtles
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
