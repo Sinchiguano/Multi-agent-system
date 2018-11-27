6a6c4783de4defef039d644547e78b2bb1ac0a15
Sinchiguano <cesarsinchiguano@hotmail.es>
2018-11-27 12:30:23 +0100

    paper1

---
+++ build/rosnet/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/casch/turtle/src/rosnet
+# Build directory: /home/casch/turtle/build/rosnet
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
